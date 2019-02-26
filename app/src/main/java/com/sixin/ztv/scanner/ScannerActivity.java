1af818dba685f27d9e8ebe723125c8de8b3f6350
PhysicalOriginal <13645532096@163.com>
2019-02-26 11:35:11 +0800

    继承Zxing

--- app/src/main/java/com/sixin/ztv/scanner/ScannerActivity.java
+++ app/src/main/java/com/sixin/ztv/scanner/ScannerActivity.java
@@ -1,15 +1,26 @@
 package com.sixin.ztv.scanner;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.zxing.Result;
+import com.google.zxing.client.result.ParsedResult;
 import com.gyf.barlibrary.ImmersionBar;
 import com.sixin.ztv.R;
 import com.sixin.ztv.base.BaseActivity;
+import com.sixin.zxing.OnScannerCompletionListener;
+import com.sixin.zxing.ScannerView;
 
-public class ScannerActivity extends BaseActivity {
+import butterknife.BindView;
+
+public class ScannerActivity extends BaseActivity implements OnScannerCompletionListener {
+
+    @BindView(R.id.sv_scanner)
+    ScannerView mSvScanner;
 
     @Override
     protected int getLayoutId() {
@@ -27,6 +38,12 @@ public class ScannerActivity extends BaseActivity {
     @Override
     protected void initView() {
         configToolbar();
+        configScanner();
+    }
+
+    private void configScanner() {
+        mSvScanner.setOnScannerCompletionListener(this);
+        mSvScanner.setLaserFrameSize(200, 200);
     }
 
     private void configToolbar() {
@@ -36,4 +53,20 @@ public class ScannerActivity extends BaseActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        mSvScanner.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mSvScanner.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onScannerCompletion(Result rawResult, ParsedResult parsedResult, Bitmap barcode) {
+
+    }
 }
